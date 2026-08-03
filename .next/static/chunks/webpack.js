172db9541ceb9457133bfecbad2faa3a606a1693
Harsh Maurya <mauryah535@gmail.com>
2026-08-03 17:19:52 -0400

    Implement new Featurs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "86a84f9135afdaad"; }
+/******/ 		__webpack_require__.h = function() { return "47843cf93bdb7882"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.913726afa3587a38.hot-update.js
+++ .next/static/webpack/webpack.913726afa3587a38.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "86a84f9135afdaad"; }
+/******/ 	__webpack_require__.h = function() { return "47843cf93bdb7882"; }
 /******/ }();
 /******/ 
 /******/ }
